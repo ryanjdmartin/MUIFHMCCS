c7ddd2f071378607b38990b8c8b16c111c4505c2
peter <engp@mcmaster.ca>
2015-02-22 19:42:32 -0500

    trying something...

--- interface/laravel/app/views/home.blade.php
+++ interface/laravel/app/views/home.blade.php
@@ -1,21 +1,12 @@
 @extends('home-layout')
 
 @section('main-view')
-
-
-   
-
     <div id="ajaxTest">
     </div>
-    	
 
 	  <script type="text/javascript">
 	    $(document).ready(function(){
 	      $('#ajaxTest').load("{{ URL::to('/buildings') }}");
 	    });
 	  </script>  	
-  
-
-
-  
 @endsection
