35d56aecff18b653785669b4cb9565eea4237e64
peter <engp@mcmaster.ca>
2015-02-25 12:22:39 -0500

    nav bar stuff not complete yet

--- interface/laravel/app/views/navbars/main-nav.blade.php
+++ interface/laravel/app/views/navbars/main-nav.blade.php
@@ -1,13 +1,26 @@
 <div class="navbar navbar-inverse main-nav">
   <div class="navbar-left">
     <ul class ="nav navbar-nav">
+      <?php //initialize id vars
+        $building_id = "";
+        $room_id = "";
+        $hood_id = "";
+      ?>
       @if($level == 'buildings')
-        Buildings
+        <li><a href ="#";> Buildings</a></li>
       @elseif($level == 'rooms')
-        <li><a id = 'buildings' href ="#";> Buildings</a></li>
+        <!--Recieves building parent object -->
+        <?php $building_id = $building->id;
+              $building_abbv = $building->abbv;
+        ?>
+        <li><a id = 'buildings' href ="#";> Buildings /</a></li> 
+        <li><a id = 'rooms_in' href ="#";> {{$building_abbv;}} </a></li>
       @elseif($level == 'fumehoods')
+        <!--Recieves room parent object-->
+        <?php $building = Building::findOrFail($building_id);?>
         <li><a id = 'buildings' href ="#";> Buildings</a></li>
       @elseif($level == 'hood')
+        <!--Recieves hood parent object-->
         <li><a id = 'buildings' href ="#";> Buildings</a></li>
       @endif
       <script type = 'text/javascript'>
@@ -16,6 +29,15 @@
           //alert('We click');
           $('#mainInfo').load("{{ URL::to('/buildings') }}");
         });
+        $('#rooms_in').on('click', function(){
+          //alert('We click');
+          $('#mainInfo').load("{{ URL::to('/rooms').'/'.$building_id }}");
+        });
+        $('#hoods_in').on('click', function(){
+          //alert('We click');
+          $('#mainInfo').load("{{ URL::to('/fumehoods').'/'.$room_id }}");
+        });
+
       });
       </script>
    </ul>
