cb3a118eb4265348163ee63a76b24f01504aea45
peter <engp@mcmaster.ca>
2015-02-25 23:13:16 -0500

    Added crappy tables as list views

--- interface/laravel/app/views/buildings.blade.php
+++ interface/laravel/app/views/buildings.blade.php
@@ -4,7 +4,40 @@
     @if(Session::get('isTileView') == 1)
       <div class="spinner-container" id="spinner" ></div>
     @else
-        LIST VIEW
+    	<?php $fumehoods = FumeHood::all();?>
+    	<table class = 'table'>
+			<tr>
+			 	<th>id</th>
+			 	<th>name</th>
+			 	<th>room_id</th>
+			 	<th>model</th>
+			 	<th>install_date</th>
+			 	<th>maintenance_date</th>
+			 	<th>notes</th>
+		 	</tr>
+			@foreach($fumehoods as $fumehood)
+	
+		 		<tr>
+			 		<td>{{$fumehood->id;}}</td>
+			 		<td><a id = "fumehood{{$fumehood->id}}" href = '#' >{{$fumehood->name;}}</a></td>
+			 		<td>{{$fumehood->room_id;}}</td>
+			 		<td>{{$fumehood->model;}}</td>
+			 		<td>{{$fumehood->install_date;}}</td>
+			 		<td>{{$fumehood->maintenence_date;}}</td>
+			 		<td>{{$fumehood->notes;}}</td>
+			 	</tr>
+		 		<script type = 'text/javascript'>
+				$(document).ready(function(){
+					$('{{"#fumehood".$fumehood->id}}').on('click', function(){
+						var url = "{{ URL::to('/hood/').'/'.$fumehood->id}}";
+						$.get(url, '', function(data){
+							$('#mainInfo').html(data);
+						});
+					});
+				});
+				</script>
+		 	@endforeach
+	 	</table>
     @endif
   </div>
 </div>
--- interface/laravel/app/views/rooms.blade.php
+++ interface/laravel/app/views/rooms.blade.php
@@ -4,7 +4,48 @@
     @if(Session::get('isTileView') == 1)
       <div class="spinner-container" id="spinner" ></div>
     @else
-        LIST VIEW
+      <?php $fumehoods = DB::table('fume_hoods')            
+                        ->leftJoin('rooms', 'rooms.id', '=', 'fume_hoods.room_id')
+                        ->where('rooms.building_id', '=', $building->id)
+                        ->select('fume_hoods.id', 'fume_hoods.name', 'fume_hoods.room_id',
+                            'fume_hoods.model', 'fume_hoods.install_date', 
+                            'fume_hoods.maintenence_date', 'fume_hoods.notes' )
+                        ->get();
+
+      //$fumehoods = FumeHood::where('room_id', =);?>
+      <table class = 'table'>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>room_id</th>
+        <th>model</th>
+        <th>install_date</th>
+        <th>maintenance_date</th>
+        <th>notes</th>
+      </tr>
+      @foreach($fumehoods as $fumehood)
+  
+        <tr>
+          <td>{{$fumehood->id;}}</td>
+          <td><a id = "fumehood{{$fumehood->id}}" href = '#' >{{$fumehood->name;}}</a></td>
+          <td>{{$fumehood->room_id;}}</td>
+          <td>{{$fumehood->model;}}</td>
+          <td>{{$fumehood->install_date;}}</td>
+          <td>{{$fumehood->maintenence_date;}}</td>
+          <td>{{$fumehood->notes;}}</td>
+        </tr>
+        <script type = 'text/javascript'>
+        $(document).ready(function(){
+          $('{{"#fumehood".$fumehood->id}}').on('click', function(){
+            var url = "{{ URL::to('/hood/').'/'.$fumehood->id}}";
+            $.get(url, '', function(data){
+              $('#mainInfo').html(data);
+            });
+          });
+        });
+        </script>
+      @endforeach
+    </table>
     @endif
   </div>
 </div>
