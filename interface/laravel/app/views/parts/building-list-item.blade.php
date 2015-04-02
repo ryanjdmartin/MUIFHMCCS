869094ef9991d7ba7d71edd275bf7a3d76ea6dd9
peter <engp@mcmaster.ca>
2015-04-01 21:23:50 -0400

    missed a file

---
+++ interface/laravel/app/views/parts/building-list-item.blade.php
@@ -0,0 +1,36 @@
+	<td><a id = "building{{$fumehood->building_id}}" href = '#' >{{$fumehood->building_name}}</a></td>
+	<td><a id = "room{{$fumehood->room_id}}" href = '#'>{{$fumehood->room_name;}}</a></td>
+	<td><a id = "fumehood{{$fumehood->id}}" href = '#' >{{$fumehood->name;}}</a></td>
+	<td>
+	    @if ($notifications['critical'])
+	      <span class="badge danger"><span class="glyphicon glyphicon-exclamation-sign"></span></span>
+	    @elseif ($notifications['alert'])
+	      <span class="badge warning"><span class="glyphicon glyphicon-info-sign"></span></span>
+	    @else
+	      <span class="badge opt"><span class="glyphicon glyphicon-ok-circle"></span></span>
+	    @endif
+	</td>
+	<td> m/s</td>
+	<td>{{$fumehood->notes;}}</td>
+<script type = 'text/javascript'>
+$(document).ready(function(){
+	$('{{"#building".$fumehood->building_id}}').on('click', function(){
+        var url = "{{ URL::to('/rooms/').'/'.$fumehood->building_id}}";
+        $.get(url, '', function(data){
+          $('#mainInfo').html(data);
+        });
+      });				
+	$('{{"#room".$fumehood->room_id}}').on('click', function(){
+        var url = "{{ URL::to('/fumehoods/').'/'.$fumehood->room_id}}";
+        $.get(url, '', function(data){
+          $('#mainInfo').html(data);
+        });
+      });
+	$('{{"#fumehood".$fumehood->id}}').on('click', function(){
+		var url = "{{ URL::to('/hood/').'/'.$fumehood->id}}";
+		$.get(url, '', function(data){
+			$('#mainInfo').html(data);
+		});
+	});
+});
+</script>
